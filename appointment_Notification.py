8408eeb10b8e8fc1fee7a133bc6052b9addb2c23
Storm226 <a.house1997@gmail.com>
2026-05-23 14:05:49 -0600

    now include time till

--- appointment_Notification.py
+++ appointment_Notification.py
@@ -1,41 +1,68 @@
 import pandas as pd
 import requests
-from datetime import date
+from datetime import datetime, date
+
+
+def format_timedelta(td):
+    total_seconds = int(td.total_seconds())
+
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+
+    return f"{hours} hours {minutes} minutes"
 
 
 # bot username: apt226bot
 
 # Telegram info
 TOKEN = "8794816467:AAHA01In7gn9-H69G5V68LdK9DBqAB5hrjQ"
-CHAT_ID = "8662593057"
+## CHAT_ID = "8662593057"
 
 # Read appointments
-df = pd.read_csv("ex.csv")
+apts_DF = pd.read_csv("ex.csv")
 
-# Load Telegram users
-users_df = pd.read_csv("users.csv")
-
-print(users_df.columns)
+# Load Telegram users, read their chatIDs
+users_DF = pd.read_csv("users.csv")
 
 # Today's date
 today = str(date.today())
 
-# Filter today's appointments
-today_appts = df[df["date"] == today]
+# Filter today's appointments -> GET THE APTS WE'RE INTERESTED IN 
+# We don't have to just do today's appointments
+# We could easily get tomorrow's, or all of this week, etc etc
 
+# Current time
+now = datetime.now()
+
+# Filter today's appointments
+today_appts = apts_DF[apts_DF["date"] == today].copy()
+
+# Combine date + time columns into full datetime
+today_appts["appointment_datetime"] = pd.to_datetime(
+    today_appts["date"] + " " + today_appts["time"]
+)
+
+today_appts["time_till"] = (
+    today_appts["appointment_datetime"] - now
+)
+
+today_appts["time_till_str"] = today_appts["time_till"].apply(format_timedelta)
+
+## IF there are any apts we want, send a text
 if len(today_appts) > 0:
 
     # Build message
-    message = "Today's appointments:\n\n"
+    message = "Hello, audrey here, here are your interesting appointments coming up!: \n"
+    message += "Today's appointments:\n\n"
 
     for _, row in today_appts.iterrows():
-        message += f"• {row['name']} — {row['time']}\n"
+        message += f"Appointment in {row['time_till_str']}"
 
     # Telegram endpoint
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
     # Send to every user
-    for _, user in users_df.iterrows():
+    for _, user in users_DF.iterrows():
 
         chat_id = str(user["chatID"])
 
